cb1d04e5f6ac224654ce5f8877ade906cb96f5a2
Loafy <kannenbergkalle@gmail.com>
2025-06-08 20:43:01 +0200

    Update LoafUtils.js

--- LoafUtils.js
+++ LoafUtils.js
@@ -1,6 +1,1 @@
-elements.hello_worldddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd = {
-	color: "#ff0000",
-	behavior: behaviors.WALL,
-	category: "land",
-	state: "solid",
-}
+
