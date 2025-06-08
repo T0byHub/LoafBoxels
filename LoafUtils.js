7965e5864b23cade9a15a62d22d84df2830dafb0
ChaosTastisch <kannenbergkalle@gmail.com>
2025-06-08 19:18:34 +0200

    Update LoafUtils.js

--- LoafUtils.js
+++ LoafUtils.js
@@ -1,11 +1,6 @@
-elements.my_element = {
-    color: "#ff7700",
-    behavior: behaviors.WALL,
-    category: "Solids",
-    state: "solid",
-    density: 2500,
-    temp: 25,
-    reactions: {
-        "water": { elem1: "steam", elem2: "stone" }
-    }
-};
+elements.hello_world = {
+	color: "#ff0000",
+	behavior: behaviors.WALL,
+	category: "land",
+	state: "solid",
+}
